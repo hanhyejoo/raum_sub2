fcb7c270b6240221e8a43dcb91d66f0086adfda7
hj <96101620+hanhyejoo@users.noreply.github.com>
2022-01-26 15:56:01 +0900

    Delete script directory

--- script/popup.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-  $('.popup a').click(function(){
-    $('.popup').hide(); //hide대신 fadeOut 사용 가능 
-  });
-});
--- script/script3.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$("#board li").click(function(){
-		$(this).children("div").slideToggle();
-		$(this).siblings().children("div").slideUp();
-	});
-});
